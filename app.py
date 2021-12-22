3b5fa3c85a794543b6b3560248823299dfbb2d3c
iml-tb-oc <96236667+iml-tb-oc@users.noreply.github.com>
2021-12-22 17:18:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,70 @@ import nltk
 nltk.download('wordnet')
 nltk.download('punkt')
 from nltk.stem import WordNetLemmatizer
-  
+import numpy as np
+import pandas as pd
+import re
+from bs4 import BeautifulSoup
+from langdetect import detect
+import nltk
+from nltk.corpus import stopwords
+import spacy
 
 import flask
+
 app = Flask(__name__)
-clf = joblib.load('quora_model.pkl')
-count_vect = joblib.load('quora_vectorizer.pkl')
+
+model_path = "models/"
+vectorizer = joblib.load(model_path + "tfidf_vectorizer.pkl", 'r')
+multilabel_binarizer = joblib.load(model_path + "multilabel_binarizer.pkl", 'r')
+model = joblib.load(model_path + "logit_nlp_model.pkl", 'r')
     
 ###################################################
-def pre_processing(text):
-    lemmatizer = WordNetLemmatizer()
-    text = text.lower()
-    text = re.sub('[0-9]+','num',text)
-    word_list = nltk.word_tokenize(text)
-    word_list =  [lemmatizer.lemmatize(item) for item in word_list]
-    return ' '.join(word_list)
+
+def garder_nom(x):
+    text = []
+    for token in x:
+        if token.pos_ in ["NOUN","PROPN"]:
+            text.append(token.text)
+    text=" ".join(text)
+    text = text.lower().replace("c #", "c#")
+    return text
+
+
+def text_cleaner(x):
+    # Remove POS not in "NOUN", "PROPN"
+    nlp = spacy.load('en_core_web_sm', exclude=['tok2vec', 'ner', 'parser', 'attribute_ruler', 'lemmatizer'])
+    x=nlp(x)
+    x=garder_nom(x)
+    # Case normalization
+    x = x.lower()
+    # Remove unicode characters
+    x = x.encode("ascii", "ignore").decode()
+    # Remove English contractions
+    x = re.sub("\'\w+", '', x)
+    # Remove ponctuation but not # (for C# for example)
+    x = re.sub('[^\\w\\s#]', '', x)
+    # Remove links
+    x = re.sub(r'http*\S+', '', x)
+    # Remove numbers
+    x = re.sub(r'\w*\d+\w*', '', x)
+    # Remove extra spaces
+    x = re.sub('\s+', ' ', x)
+        
+    # Tokenization
+    x = nltk.tokenize.word_tokenize(x)
+    # List of stop words in select language from NLTK
+    stop_words = stopwords.words("english)
+    # Remove stop words
+    x = [word for word in x if word not in stop_words 
+         and len(word)>2]
+    # Lemmatizer
+    wn = nltk.WordNetLemmatizer()
+    x = [wn.lemmatize(word) for word in x]
+    
+    # Return cleaned text
+    return x
+###################
 ###################################################
 
 
@@ -59,7 +108,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    #clf = joblib.load('quora_model.pkl')
-    #count_vect = joblib.load('quora_vectorizer.pkl')
     app.run(debug=True)
-    #app.run(host='localhost', port=8081)
+    
